089ce9ee45e54aa26d61f6f977fd15affacfafd4
HanJeongSeol <hanjeong94@naver.com>
2023-02-26 15:02:49 +0900

    #22 Fix : 클래스를 사용하여 Tk 객체 생성
    생성자를 통해 초기값 설정 후 객체 생성

--- main.py
+++ main.py
@@ -32,71 +32,40 @@ class PororoOcr:
             self.show_img_with_ocr()
 
         return ocr_text
-    
 
-class Capture(PororoOcr) :
-    def getFileName():
-        localTime = time.localtime()
-        nowTime = "%04d-%02d-%02d" % (localTime.tm_year, localTime.tm_mon, localTime.tm_mday)
-        return nowTime
+def captureWidget():
+    class CaptureGUI :
+        def __init__(self,master) :
+            self.master = master
+            master.geometry("430x420+800+400")
+            master.attributes('-alpha',0.8)
+            master.attributes('-topmost',1)
+            master.bind("<Configure>", self.setSize)
 
-            
-    def capture(root, roi=[]) : 
-        po = PororoOcr()
-        global cnt
-        root.attributes('-alpha',0.0)
-        root.update()
-        while True :
-            startPoint = time.time()
-            cnt = cnt+1
-            img = ImageGrab.grab(roi)
-            frame = np.array(img)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img_name = "image/talk_%s_%d.png" % (Capture.getFileName(),cnt)
-            cv2.imwrite(img_name,frame)
-            text = po.run_ocr(img_path=img_name)
-            print(text)
-            endPoint=time.time()
-            print("시간 : " , endPoint - startPoint)
-            key = cv2.waitKey(1)
+        def setSize(self,event):
+            time.sleep(0.001)
 
-    def destroy(root) :
-        root.destroy()
-        return
-
-def setSize(event):
-    time.sleep(0.001)
-
-    global coordinate 
-    coordinate = list()
+            global coordinate 
+            coordinate = list()
 
 
-    width = root.winfo_width()
-    height = root.winfo_height()
+            width = root.winfo_width()
+            height = root.winfo_height()
 
-    coordinate.append(root.winfo_rootx())
-    coordinate.append(root.winfo_rooty())
-    coordinate.append(coordinate[0] + width)
-    coordinate.append(coordinate[1]+height)
+            coordinate.append(root.winfo_rootx())
+            coordinate.append(root.winfo_rooty())
+            coordinate.append(coordinate[0] + width)
+            coordinate.append(coordinate[1]+height)
 
-    head = '캡쳐영역' + ' ' + str(width) + ' x ' + str(height) + ' ' + str(coordinate[0]) + ' x ' + str(coordinate[1])
-    root.title(head)
+            head = '캡쳐영역' + ' ' + str(width) + ' x ' + str(height) + ' ' + str(coordinate[0]) + ' x ' + str(coordinate[1])
+            root.title(head)
 
-    time.sleep(0.01)
+            time.sleep(0.01)
 
+    root = Tk()
+    captureWidget = CaptureGUI(root)
+    root.mainloop()
 
-root = Tk()
-root.title("캡처영역")
-root.attributes('-alpha',0.8)
-root.attributes('-topmost',1)
-root.geometry("430x420+800+400")
-
-
-root.bind("<Configure>" ,setSize)
-
-font=tkinter.font.Font(size=20, weight ='bold', underline=True)
-
-setSize(None)
-
-root.mainloop()
+if __name__ == "__main__" :
+    captureWidget()
 
