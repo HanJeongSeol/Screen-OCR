201e2486bc46ee12b7071e9be58a56bedfa02196
HanJeongSeol <hanjeong94@naver.com>
2023-03-03 12:39:04 +0900

    #24 Feat : 소켓 클라이언트 구현
    서버로 추출한 텍스트 데이터를 전송하기 위해 python-socketio 모듈을 사용하여 소켓 클라이언트 구현

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import time
 import threading
 from tkinter import *
-import tkinter.font
 from pororo import Pororo
 import numpy as np
 import cv2
 from PIL import ImageGrab
-
+import socketio
 
 global cnt
 cnt = 0
+
+class MyClient:
+    def __init__(self):
+        self.sio = socketio.Client()
+        self.sio.connect('http://localhost:3306')
+
+        @self.sio.on('connect')
+        def on_connect():
+            print('서버 연결')
+        
+        @self.sio.on('check')
+        def on_chek(data):
+            print("서버로 전달한 데이터 확인 %s" % data)
+        
+        @self.sio.on('state')
+        def on_state(data):
+            print("소켓 연결 상태 : %s" %data)
+    
+    def send_data(self,data):
+        self.sio.emit('text', data)
+
+    def disconnect(self):
+        self.sio.disconnect()
+            
 
 class PororoOcr:
     def __init__(self, model: str = "brainocr", lang: str = "ko", **kwargs):
@@ -112,10 +135,12 @@ def captureWidget():
             root.update()
             global cnt
             poro_ocr = PororoOcr()
+            my_client = MyClient()
             while True :
                 startTime = time.time()
                 try :
                     if not isRunningFunc() :
+                        my_client.disconnect()
                         return
                 except : pass
                 pass
@@ -127,7 +152,7 @@ def captureWidget():
                 img_name = "image/talk%d_test.png" % cnt
                 cv2.imwrite(img_name,frame)
                 text = poro_ocr.run_ocr(img_path=img_name)
-                print(text)
+                my_client.send_data(text)
                 endTine = time.time()
                 print("소요시간 : " , endTine-startTime)
 
